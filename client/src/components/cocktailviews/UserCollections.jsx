35b827a6f19c5d2de7a012355f557901f9590ec3
rachel-howell <rhowell96@gmail.com>
2023-03-19 18:11:57 -0700

    Add view user collections component

---
+++ client/src/components/cocktailviews/UserCollections.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import CocktailCollection from './CocktailCollection';
+import axios from 'axios'
+
+
+const ViewAllCollections = () => {
+
+    const [ collectionList, setCollectionList ] = useState([]);
+    let url = 'http://localhost:8000/api/viewcollection/'
+
+    useEffect(()=>{
+        axios
+            .get(`http://localhost:8000/api/viewcollections`)
+            .then((res)=>(setCollectionList(res.data)))
+            .catch(err=>console.log(err))
+    }, [])
+
+  return (
+    <div>
+        <h1>Browse All Collections</h1>
+        {
+            collectionList.map(collection =>(
+            <div>
+                <CocktailCollection url={`http://localhost:8000/api/viewcollection/${collection.key}`} title={collection.title} numToDisplay={6}/>
+            </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default ViewAllCollections
